2d12b8726222c640f8a6dd4bcb8054df030b6c0c
sujatha-94 <sujathakarri701@gmail.com>
2024-07-11 12:54:05 +0530

    hhiiii

--- app.java
+++ app.java
@@ -4,11 +4,11 @@ public class app {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         
-        Team3 sTeam3 = new Team3();
+        team3 sTeam3 = new team3();
         sTeam3.sujatha();
-        Team3 jTeam3= new Team3();
+        team3 jTeam3= new team3();
         jTeam3.swarupa();
-        Team3 kTeam3= new Team3();
+        team3 kTeam3= new team3();
         kTeam3.kavitha();
     }
 }
