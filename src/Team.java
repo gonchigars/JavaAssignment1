9604b5a12f8963a4a6e007b4590493f4508ee9bf
Ritesh Kumar <riteshkumarbehera43@gmail.com>
2024-07-11 12:54:44 +0530

    Add files via upload

---
+++ src/Team.java
@@ -0,0 +1,9 @@
+public class Team {
+    public void name(){
+        System.out.println("Ritesh Kumar");
+        System.out.println("Venkat");
+        System.out.println("Sai Kumar");
+        System.out.println("Goutam");
+
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        Team team2 = new Team();
+        team2.name();
+    }
+
+    
+}
