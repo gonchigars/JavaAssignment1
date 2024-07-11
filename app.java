170c03e7b46329736873dd218256ae0bef57bba5
sujatha-94 <sujathakarri701@gmail.com>
2024-07-11 12:49:56 +0530

    hello

---
+++ app.java
@@ -0,0 +1,14 @@
+package JavaAssignment1;
+
+public class app {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        
+        Team3 sTeam3 = new Team3();
+        sTeam3.sujatha();
+        Team3 jTeam3= new Team3();
+        jTeam3.swarupa();
+        Team3 kTeam3= new Team3();
+        kTeam3.kavitha();
+    }
+}
---
+++ team3.java
@@ -0,0 +1,13 @@
+package JavaAssignment1;
+
+public class team3 {
+    public void sujatha() {
+        System.out.println("sujatha");
+    }
+    public void swarupa() {
+        System.out.println("swarupa");
+    }
+    public void kavitha() {
+        System.out.println("kavitha");
+    }
+}
