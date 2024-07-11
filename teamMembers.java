6deebb3f52c3d1b31d88437553b283f4e770d83f
Verat-hub <venkey482thalka@gmail.com>
2024-07-12 01:43:52 +0530

    Hello_dear

---
+++ teamMembers.java
@@ -0,0 +1,15 @@
+public class teamMembers {
+    public void Venkat() {
+        System.out.println("Venkat");
+    }
+        public void Ritesh() {
+        System.out.println("Ritesh");
+           }
+              public void Goutam() {
+        System.out.println("Goutam");
+              }
+              public void SaiKumar() {
+        System.out.println("SaiKumar");   
+    }
+}
+        
