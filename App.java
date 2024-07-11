caf069353275bb7d17aa8d1dcbda1a1fe0d211f5
jangamswarupa-595 <swarupajangam9@gmail.com>
2024-07-11 23:05:55 +0530

    Hello

---
+++ App.java
@@ -0,0 +1,13 @@
+public class App {
+        public static void main(String[] args) throws Exception {
+            System.out.println("Team3");
+            Team3 STeam3=new Team3();
+            STeam3.Swarupa();
+            Team3 steam=new Team3();
+            steam.Sujatha();
+            Team3 kteam=new Team3();
+            kteam.Kavitha();
+    
+    
+        }
+    }
