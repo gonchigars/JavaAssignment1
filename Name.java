9f029a86e2f6139ffaa5c8809a3f5fd6b7750639
ganeshteki <ganeshteki2001@gmail.com>
2024-07-14 14:53:01 +0530

    gn

---
+++ Name.java
@@ -0,0 +1,11 @@
+package JavaAssignment1;
+
+public class Name {
+    public void work()
+    {
+        System.out.println("first");
+        System.out.println("second");
+        System.out.println("third");
+    }
+    
+}
