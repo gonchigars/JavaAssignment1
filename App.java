2af39a9f803d38ab22a787fa1d3a445a8aa2517e
SriSankeerthanaGorrela <112100515+SriSankeerthanaGorrela@users.noreply.github.com>
2024-07-14 22:25:40 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Team T1=new Team();
+        T1.name1();
+        Team T2=new Team();
+        T2.name2();
+        Team T3=new Team();
+        T2.name3();
+        Team T4=new Team();
+        T2.name4();
+    }
+}
