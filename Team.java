e43ecdbae393a5c765677a8d5a5b406581129d24
syamkumarnallamilli <167566145+syamkumarnallamilli@users.noreply.github.com>
2024-07-15 00:00:36 +0530

    Add files via upload

---
+++ Team.java
@@ -0,0 +1,11 @@
+public class Team {
+    public void name(){
+        System.out.println("syam");
+        System.out.println("sri");
+        System.out.println("ganesh");
+        System.out.println("pavani");
+        System.out.println("sankeerthana");
+        
+        
+    }
+}
